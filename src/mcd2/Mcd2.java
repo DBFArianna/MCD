160e88a4cb4ec4954534cb53d038fa57d8bad6db
Corso <Corso@PC-037.homenet.telecomitalia.it>
2018-10-17 09:32:43 +0200

    Aggiunte Postcondizioni

--- src/mcd2/Mcd2.java
+++ src/mcd2/Mcd2.java
@@ -4,7 +4,7 @@ public class Mcd2 {
 
     public static void main(String[] args) {
         try {
-            System.out.println(mcd(0, 21));
+            System.out.println(mcd(45, 21));
         } catch (ExceptionMinore e) {
 //            NO SE DEVO CONSEGNARE AD UN UNTENTE!!!!!
             System.exit(45);
@@ -25,9 +25,24 @@ public class Mcd2 {
             return b;
         } else {
             if (a >= b) {
+//                postcondizioni
                 mcd = mcd(b, a % b);
+                
+                if (mcd < 0) {
+                    throw new ExceptionMinore(3);
+                }
+                if (!(a % mcd == 0 && b % mcd == 0)) {
+                    throw new ExceptionMinore(4);
+                }
             } else {
+//                postcondizioni
                 mcd = mcd(a, b % a);
+                if (mcd < 0) {
+                    throw new ExceptionMinore(3);
+                }
+                if (!(a % mcd == 0 && b % mcd == 0)) {
+                    throw new ExceptionMinore(4);
+                }
             }
         }
         return mcd;
@@ -43,6 +58,14 @@ public class Mcd2 {
                     break;
                 case 2:
                     s = "secondo parametro negativo";
+                    break;
+                case 3:
+                    System.out.println("Metodo MCD: valore mcd negativo"); 
+                    System.exit(46);
+                    break;
+                case 4:
+                    System.out.println("Metodo MCD: Resto diverso da 0");
+                    System.exit(54);
                     break;
                 default:
                     s = "Eccezione non getita";
